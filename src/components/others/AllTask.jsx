9103aa4f724fa63c2c23ec675ce8256d619e8be9
Danish Mahajan <dabishmahajan@gmail.com>
2025-04-22 14:12:58 +0530

    Test Screens created

---
+++ src/components/others/AllTask.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div className='bg-[#1c1c1c] p-5 rounded mt-5 h-40 overflow-auto'>
+        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Danish</h2>
+            <h2>Make a UI design </h2>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Danish</h2>
+            <h2>Make a UI design </h2>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Danish</h2>
+            <h2>Make a UI design </h2>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Danish</h2>
+            <h2>Make a UI design </h2>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-violet-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Danish</h2>
+            <h2>Make a UI design </h2>
+            <h5>Status</h5>
+        </div>
+
+    </div>
+  )
+}
+
+export default AllTask
